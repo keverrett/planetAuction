8a42826cc42143e6141ed41d032876be3699d240
kevin <keverrett1989@gmail.com>
2021-07-21 17:24:41 -0400

    setting up header and footer

--- FrontEnd/src/components/Header.js
+++ FrontEnd/src/components/Header.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header>
+            <Navbar expand="lg" variant='dark'>
+                <Navbar.Brand>Planet Auction</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                        <Nav.Link href="/">Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+
+            </Navbar>
+        </header>
     )
 }
 
